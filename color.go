78cf588f9204b1b030577cdc24352b7a912438d9
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2020-03-08 05:40:37 +0000

    Adding composer.json

--- color.go
+++ color.go
@@ -5,3 +5,19 @@ import "fmt"
 
 //Color struct, defines the color
 type Color struct{}
+
+var c Color
+
+//DarkGray color
+func (c Color) DarkGray(t string) {
+	fmt.Print("\x1b[30;1m") //dark gray
+	fmt.Println(t)
+	fmt.Print("\x1b[0m") //defaultColor
+}
+
+//Red color
+func (c Color) Red(t string) {
+	fmt.Print("\x1b[31;1m") //red
+	fmt.Println(t)
+	fmt.Print("\x1b[0m") //defaultColor
+}

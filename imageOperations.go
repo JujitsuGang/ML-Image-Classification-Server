7c20bdbe6c1c8ca7f6195ed88abd523f06e48163
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2022-02-26 15:59:42 +0000

    Implementing

--- imageOperations.go
+++ imageOperations.go
@@ -72,4 +72,6 @@ func imageToHistogram(img image.Image) [][]float64 {
 
 func Resize(img image.Image) image.Image {
 	r := resize.Resize(uint(config.ImgWidth), uint(config.ImgHeigh), img, resize.Lanczos3)
-	return
+	return r
+}
+func Edge

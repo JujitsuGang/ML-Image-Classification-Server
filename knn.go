e0b785e11b5b5f34bb6e1bf4bac2a8d956ad287c
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2022-04-13 18:01:03 +0000

    Removed unused file

--- knn.go
+++ knn.go
@@ -13,3 +13,28 @@ type Neighbour struct {
 }
 
 func euclideanDist(img1, img2 [][]float64) float64 {
+	var dist float64
+	for i := 0; i < len(img1); i++ {
+		for j := 0; j < len(img1[i]); j++ {
+			dist += (img1[i][j] - img2[i][j]) * (img1[i][j] - img2[i][j])
+		}
+	}
+
+	return dist
+}
+
+func isNeighbour(neighbours []Neighbour, dist float64, label string) []Neighbour {
+	var temp []Neighbour
+
+	for i := 0; i < len(neighbours); i++ {
+		temp = append(temp, neighbours[i])
+	}
+	ntemp := Neighbour{dist, label}
+	temp = append(temp, ntemp)
+
+	//now, sort the temp array
+	sort.Slice(temp, func(i, j int) bool {
+		return temp[i].Dist < temp[j].Dist
+	})
+
+	for i := 0; i < len(neighbours); i++ {

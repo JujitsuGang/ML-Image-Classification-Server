88d99e659445eeeb82fb7e6e7312d9c6daee1d7e
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2022-10-07 15:18:39 +0000

    Working commit.

--- readDataset.go
+++ readDataset.go
@@ -63,3 +63,4 @@ func readDataset(path string) []Dataset {
 		folderFiles, _ := ioutil.ReadDir(path + "/" + folder.Name())
 		for _, file := range folderFiles {
 			//get the image as original
+			image := r

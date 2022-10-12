7b5c2943c33e8e524bcba701497cb5b7add5697d
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2022-10-12 16:37:09 +0000

    Updated javadoc.

--- readDataset.go
+++ readDataset.go
@@ -63,4 +63,5 @@ func readDataset(path string) []Dataset {
 		folderFiles, _ := ioutil.ReadDir(path + "/" + folder.Name())
 		for _, file := range folderFiles {
 			//get the image as original
-			image := readImage(path + "/" + folder.Name() + "/" + file.Name()
+			image := readImage(path + "/" + folder.Name() + "/" + file.Name())
+			histogram

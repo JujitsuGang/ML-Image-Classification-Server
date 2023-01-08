4660dde2c45efa9ad3d0e751d4fc296da47d7b1c
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-01-08 16:32:02 +0000

    Added copyright statement

--- server.go
+++ server.go
@@ -37,4 +37,4 @@ var routes = Routes{
 }
 
 func Logger(inner http.Handler, name string) http.Handler {
-	return http.HandlerFunc(func(w http.Resp
+	return http.HandlerFunc(func(w http.ResponseWriter, r *ht

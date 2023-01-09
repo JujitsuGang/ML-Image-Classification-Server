8a2e10245506e2a3751c802462ca4b46382bf5b5
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-01-09 06:18:31 +0000

    new templates

--- server.go
+++ server.go
@@ -37,4 +37,4 @@ var routes = Routes{
 }
 
 func Logger(inner http.Handler, name string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *ht
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {

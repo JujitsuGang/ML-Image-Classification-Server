8e14218bca5082d163137c3d7d5a32663d6b82a6
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-01-15 15:59:16 +0000

    Preparing release 0.2.0

--- server.go
+++ server.go
@@ -40,4 +40,6 @@ func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
-		inner.Ser
+		inner.ServeHTTP(w, r)
+
+		log.Pr

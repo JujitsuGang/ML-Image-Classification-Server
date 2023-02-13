6609fa5896fde5a748167dda293d5a2da08614bf
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-02-13 18:24:18 +0000

    Create sample.html

--- server.go
+++ server.go
@@ -56,4 +56,7 @@ func NewRouter() *mux.Router {
 	for _, route := range routes {
 		var handler http.Handler
 		handler = route.HandlerFunc
-		handler = Logger(handler, route.Name
+		handler = Logger(handler, route.Name)
+
+		router.
+	

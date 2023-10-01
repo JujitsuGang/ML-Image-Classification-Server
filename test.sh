4934a20f144ba72336c297a254eec548919162f2
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-10-01 08:49:30 +0000

    Added new Language, Kinyarwanda (rw), to this package

--- test.sh
+++ test.sh
@@ -8,3 +8,18 @@ echo "sending leopard to server"
 echo "server response:"
 curl -F file=@./testleopard2.png http://127.0.0.1:3055/image
 echo ""
+
+echo "sending leopard to server"
+echo "server response:"
+curl -F file=@./testleopard3.png http://127.0.0.1:3055/image
+echo ""
+
+echo "sending leopard to server"
+echo "server response:"
+curl -F file=@./testleopard4.png http://127.0.0.1:3055/image
+echo ""
+
+echo "sending laptop to server"
+echo "server response:"
+curl -F file=@./testlaptop1.png http://127.0.0.1:3055/image
+echo ""
